a012f24bf2d4819021faa21bc8a05945a4edfa05
Kirill Borisov <lensvol@gmail.com>
2018-09-17 20:43:14 +0200

    Replaced 'print' in strip parsers with internal log method.

--- telestrip/comic_strips.py
+++ telestrip/comic_strips.py
@@ -31,7 +31,7 @@ class ComicStrip(object):
     async def get_updates(self, moment: DateTime) -> List[Update]:
         result = []
 
-        print(f"Requesting feed from {self.INDEX_URL}...")
+        self.log(f"Requesting feed from {self.INDEX_URL}...")
         response, page = await fetch(self.INDEX_URL)
         rss = feedparser.parse(page)
 
@@ -43,7 +43,6 @@ class ComicStrip(object):
             published_on = pendulum.from_timestamp(mktime(parsed_timestamp))
 
             if published_on < moment:
-                print(f"[{self.TITLE}] Ignoring entry {entry.title}: too old")
                 continue
 
             update = await self.process_entry(entry, published_on)
@@ -54,6 +53,9 @@ class ComicStrip(object):
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
         raise NotImplemented
+
+    def log(self, message: str):
+        print(f'[{self.TITLE}] {message}')
 
 
 class PennyArcade(ComicStrip):
@@ -78,10 +80,10 @@ class PennyArcade(ComicStrip):
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
         if not entry.title.startswith("Comic:"):
-            print(f"[{self.TITLE}] Ignoring entry {entry.title}: not a comic")
+            self.log(f"Ignoring entry {entry.title}: not a comic")
             return None
 
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         response, comic_page = await fetch(entry.link)
         soup = BeautifulSoup(comic_page, "html.parser")
         comic_img = soup.find("div", {"id": "comicFrame"}).findChild("img")
@@ -90,14 +92,14 @@ class PennyArcade(ComicStrip):
 
         post_link = soup.find('a', {'title': 'Read News Post'})
         if post_link:
-            print(f"[{self.TITLE}] Fetching post page for {entry.title}...")
+            self.log(f"Fetching post page for {entry.title}...")
             response, post_page = await fetch(post_link.attrs['href'])
             soup = BeautifulSoup(post_page, 'html.parser')
             copy_text = soup.find('div', {'class': 'copy'})
             if copy_text:
                 description = self.to_telegram_markdown(copy_text)
 
-        print(f'[{self.TITLE}] Fetching image from {comic_img.attrs["src"]}')
+        self.log(f'Fetching image from {comic_img.attrs["src"]}')
         response, image = await fetch(comic_img.attrs["src"])
 
         return Update(entry.title, description, published_on, [image])
@@ -110,15 +112,15 @@ class PvP(ComicStrip):
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
         if not entry.title.startswith("Comic:"):
-            print(f"[{self.TITLE}] Ignoring entry {entry.title}: not a comic")
+            self.log(f"Ignoring entry {entry.title}: not a comic")
             return None
 
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         response, comic_page = await fetch(entry.link)
         soup = BeautifulSoup(comic_page, "html.parser")
         comic_img = soup.find("section", {"class": "comic-art"}).findChild("img")
 
-        print(f'[{self.TITLE}] Fetching image from {comic_img.attrs["src"]}')
+        self.log(f'Fetching image from {comic_img.attrs["src"]}')
         response, image = await fetch(comic_img.attrs["src"])
 
         return Update(entry.title, None, published_on, [image])
@@ -130,7 +132,7 @@ class SaturdayMorningBreakfastCereal(ComicStrip):
     INDEX_URL = "https://www.smbc-comics.com/comic/rss"
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         soup = BeautifulSoup(entry.description, "html.parser")
         comic_img = soup.find("img")
 
@@ -140,7 +142,7 @@ class SaturdayMorningBreakfastCereal(ComicStrip):
         if p_hovertext:
             description = p_hovertext.findNext('br').next
 
-        print(f'[{self.TITLE}] Fetching image from {comic_img.attrs["src"]}')
+        self.log(f'Fetching image from {comic_img.attrs["src"]}')
         response, image = await fetch(comic_img.attrs["src"])
 
         return Update(entry.title, description, published_on, [image])
@@ -152,11 +154,11 @@ class XKCD(ComicStrip):
     INDEX_URL = "https://xkcd.com/rss.xml"
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         soup = BeautifulSoup(entry.description, "html.parser")
         comic_img = soup.find("img")
 
-        print(f'[{self.TITLE}] Fetching image from {comic_img.attrs["src"]}')
+        self.log(f'Fetching image from {comic_img.attrs["src"]}')
         response, image = await fetch(comic_img.attrs["src"])
         return Update(entry.title, comic_img.attrs['alt'], published_on, [image])
 
@@ -167,14 +169,14 @@ class CommitStrip(ComicStrip):
     INDEX_URL = 'https://www.commitstrip.com/en/feed/'
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         soup = BeautifulSoup(entry.content[0].value, "html.parser")
         comic_imgs = soup.findAll("img")
 
         if not comic_imgs:
             return None
 
-        print(f'[{self.TITLE}] Fetching image from {comic_imgs[0].attrs["src"]}')
+        self.log(f'Fetching image from {comic_imgs[0].attrs["src"]}')
         response, image = await fetch(comic_imgs[0].attrs["src"])
         return Update(entry.title, '', published_on, [image])
 
@@ -185,7 +187,7 @@ class SlackWyrm(ComicStrip):
     INDEX_URL = 'http://www.joshuawright.net/rss_joshuawright.xml'
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         response, comic_page = await fetch(entry.link)
         soup = BeautifulSoup(comic_page, "html.parser")
         comic_frames = soup.findAll("div", {"data-muse-type": "img_frame"})
@@ -193,7 +195,7 @@ class SlackWyrm(ComicStrip):
         images = []
         for frame in comic_frames:
             comic_img = frame.findChild('img')
-            print(f'[{self.TITLE}] Fetching image from {comic_img.attrs["src"]}')
+            self.log(f'Fetching image from {comic_img.attrs["src"]}')
             response, image = await fetch('http://www.joshuawright.net/' + comic_img.attrs["src"])
             images.append(image)
 
@@ -221,7 +223,7 @@ class Kill6BillionDemons(ComicStrip):
         return result.replace('\0xa', '').strip()
 
     async def process_entry(self, entry, published_on: DateTime) -> Union[Update, None]:
-        print(f"[{self.TITLE}] Fetching comic page for {entry.title}")
+        self.log(f"Fetching comic page for {entry.title}")
         response, comic_page = await fetch(entry.link)
         soup = BeautifulSoup(comic_page, "html.parser")
         img_meta = soup.find('meta', {'property': 'og:image'})
